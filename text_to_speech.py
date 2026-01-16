187b6862540e0910ceaf45136f709de186705d4d
Yashdesh-mukh <yashdeshmukh621@gmail.com>
2026-01-16 10:46:56 +0530

    text to speech conflict resloved

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,11 +1,9 @@
 import pyttsx3
 
-engine = pyttsx3.init()
-engine.setProperty('rate', 230)
-
-
 def text_to_speech(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 230)
     engine.say(text)
     engine.runAndWait()
-
+    engine.stop()
 
